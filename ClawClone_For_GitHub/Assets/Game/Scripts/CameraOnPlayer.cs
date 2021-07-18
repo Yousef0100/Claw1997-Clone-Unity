f71de048d8e212fe4faf12d065768538f3a087e0
Yousef0100 <yousefabed0100@gmail.com>
2021-07-18 19:23:29 +0200

    update

---
+++ ClawClone_For_GitHub/Assets/Game/Scripts/CameraOnPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraOnPlayer : MonoBehaviour
+{
+    public Camera cam;
+    public float cameraDepthOffset;
+    public float speed;
+
+    // Start 'Borders' :
+
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    // End 'Borders'
+
+    private void Start()
+    {
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = Vector2.Lerp(new Vector2(cam.transform.position.x, cam.transform.position.y), transform.position, speed * Time.deltaTime);
+        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, -10f);
+    }
+}
